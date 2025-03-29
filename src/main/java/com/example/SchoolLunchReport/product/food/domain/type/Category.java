16ed924340d63b6019b2d2669c1ddfa3b2c33b70
0702Yoon <gkstkddbs99@naver.com>
2025-03-30 00:00:43 +0900

    [Feat] Food 타입과 엔터티 구현

--- src/main/java/com/example/SchoolLunchReport/product/food/domain/type/Category.java
+++ src/main/java/com/example/SchoolLunchReport/product/food/domain/type/Category.java
@@ -1,5 +1,7 @@
 package com.example.SchoolLunchReport.product.food.domain.type;
+
 import java.util.Arrays;
+
 public enum Category {
     RICE, SOUP, MAIN_DISH, SIDE_DISH, DESSERT;
     public static Category getInstance(String name) {
