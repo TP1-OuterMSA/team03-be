0550d731ab6d38ee871fe3ccebf0b0bd0c48c048
choieuntaek <choiet0225@gmail.com>
2025-04-04 20:24:36 +0900

    feat: update base url point

--- src/main/java/com/example/SchoolLunchReport/dummy/dataset/controller/DatasetController.java
+++ src/main/java/com/example/SchoolLunchReport/dummy/dataset/controller/DatasetController.java
@@ -1,4 +1,5 @@
 package com.example.SchoolLunchReport.dummy.dataset.controller;
+
 import static com.example.SchoolLunchReport.global.common.Constants.ANALYTICS_TEAM_URL;
 import com.example.SchoolLunchReport.dummy.dataset.dto.FeedBackDTO;
 import com.example.SchoolLunchReport.dummy.dataset.dto.MenuWithFoodsDTO;
--- src/main/java/com/example/SchoolLunchReport/statistics/rank/controller/RankController.java
+++ src/main/java/com/example/SchoolLunchReport/statistics/rank/controller/RankController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/menu")
+@RequestMapping("/api/team3/analytics/menu")
 @RequiredArgsConstructor
 public class RankController implements RankControllerDocs {
 
