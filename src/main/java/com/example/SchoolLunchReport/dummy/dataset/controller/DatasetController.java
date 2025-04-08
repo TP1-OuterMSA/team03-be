a79e653bc3796ec8e53b5a6545d8cdced23f1e58
euntaek <108321588+euntaek4187@users.noreply.github.com>
2025-04-08 19:12:51 +0900

    Feature/llm (#15)
    * feat: demo test
    
    * test 4

--- src/main/java/com/example/SchoolLunchReport/dummy/dataset/controller/DatasetController.java
+++ src/main/java/com/example/SchoolLunchReport/dummy/dataset/controller/DatasetController.java
@@ -97,7 +97,7 @@ public class DatasetController implements DataControllerDocs {
     @GetMapping("/test")
     public ResponseEntity<Map<String, String>> testEndpoint() {
         Map<String, String> response = new HashMap<>();
-        response.put("message", "test3!");
+        response.put("message", "test4 7:11");
         return ResponseEntity.ok(response);
     }
 }
