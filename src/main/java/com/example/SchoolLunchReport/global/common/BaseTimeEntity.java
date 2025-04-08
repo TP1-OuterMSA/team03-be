93996b68085a467b8006d0990503af46a7ece6fe
0702Yoon <gkstkddbs99@naver.com>
2025-04-08 12:13:09 +0900

    직접 데이터를 넣기 위해서 BaseTimeEntity의 Listener를 수정

--- src/main/java/com/example/SchoolLunchReport/global/common/BaseTimeEntity.java
+++ src/main/java/com/example/SchoolLunchReport/global/common/BaseTimeEntity.java
@@ -1,20 +1,34 @@
 package com.example.SchoolLunchReport.global.common;
 
+import com.example.SchoolLunchReport.global.common.BaseTimeEntity.CreatedAtListener;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
 import java.time.LocalDate;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
 @MappedSuperclass
-@EntityListeners(AuditingEntityListener.class)
+@EntityListeners(CreatedAtListener.class)
 public class BaseTimeEntity {
 
     @CreatedDate
     @Column(updatable = false)
     protected LocalDate createdAt;
 
+    private void setCreatedAt(LocalDate createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public static class CreatedAtListener {
+
+        @PrePersist
+        public void setCreatedAt(BaseTimeEntity entity) {
+            if (entity.getCreatedAt() == null) {
+                entity.setCreatedAt(LocalDate.now());
+            }
+        }
+    }
 }
