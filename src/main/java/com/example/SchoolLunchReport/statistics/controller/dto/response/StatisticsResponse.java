70126cc126f1066febed94d77ffe7315b8c864d8
0702Yoon <gkstkddbs99@naver.com>
2025-04-08 12:17:34 +0900

    FeedBack의 score를 double로 변경

--- src/main/java/com/example/SchoolLunchReport/statistics/controller/dto/response/StatisticsResponse.java
+++ src/main/java/com/example/SchoolLunchReport/statistics/controller/dto/response/StatisticsResponse.java
@@ -2,8 +2,6 @@ package com.example.SchoolLunchReport.statistics.controller.dto.response;
 
 import com.example.SchoolLunchReport.statistics.domain.type.PeriodType;
 import java.util.List;
-import java.util.Objects;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 
 @Builder
@@ -13,7 +11,7 @@ public record StatisticsResponse(
 ) {
 
     public record ScoreCount(
-        int score,
+        double score,
         long count
     ) {
 
