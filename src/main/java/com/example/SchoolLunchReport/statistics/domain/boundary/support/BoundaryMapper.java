c18ecb5669a03986a66cb9c9ae06757cf9de3d38
0702Yoon <gkstkddbs99@naver.com>
2025-05-01 18:26:20 +0900

    [Refactor] 태크 클래스에서, 계층 구조 클래스로 변경

---
+++ src/main/java/com/example/SchoolLunchReport/statistics/domain/boundary/support/BoundaryMapper.java
@@ -0,0 +1,49 @@
+package com.example.SchoolLunchReport.statistics.domain.boundary.support;
+
+import com.example.SchoolLunchReport.statistics.controller.dto.request.DesiredFoodRequestDto;
+import com.example.SchoolLunchReport.statistics.domain.boundary.entity.Boundary;
+import com.example.SchoolLunchReport.statistics.domain.boundary.support.calculator.BoundaryCalculator;
+import com.example.SchoolLunchReport.statistics.domain.boundary.type.PeriodType;
+import java.time.LocalDate;
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BoundaryMapper {
+
+    final Map<PeriodType, BoundaryCalculator> calculatorMap;
+
+    public BoundaryMapper(List<BoundaryCalculator> calculators) {
+
+        this.calculatorMap = new EnumMap<>(PeriodType.class);
+        for (BoundaryCalculator calc : calculators) {
+            PeriodType key = calc.supportedType();
+            if (calculatorMap.putIfAbsent(key, calc) != null) {
+                throw new IllegalStateException("Duplicate calculator for: " + key);
+            }
+        }
+    }
+
+    public Boundary mapBoundary(DesiredFoodRequestDto desiredFoodRequestDto) {
+        PeriodType periodType = desiredFoodRequestDto.periodType();
+        BoundaryCalculator boundaryCalculator = calculatorMap.get(periodType);
+        return boundaryCalculator.getBoundary(desiredFoodRequestDto);
+    }
+
+    public Boundary mapBoundary(PeriodType periodType, LocalDate conditionDate) {
+        BoundaryCalculator boundaryCalculator = calculatorMap.get(periodType);
+        return boundaryCalculator.createBoundary(conditionDate);
+    }
+
+    public LocalDate getRankRegisterDate(PeriodType periodType, LocalDate date) {
+        BoundaryCalculator boundaryCalculator = calculatorMap.get(periodType);
+        return boundaryCalculator.getRankRegisterDate(date);
+    }
+
+    public LocalDate getStartOfPreviousPeriod(PeriodType periodType, LocalDate date) {
+        BoundaryCalculator boundaryCalculator = calculatorMap.get(periodType);
+        return boundaryCalculator.getStartOfPreviousPeriod(date);
+    }
+}
