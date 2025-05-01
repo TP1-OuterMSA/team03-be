bd618e20b161f2a2573b7632a02f097a11b48096
0702Yoon <gkstkddbs99@naver.com>
2025-05-01 18:33:17 +0900

    패키지 이동으로 인한 변경

--- src/main/java/com/example/SchoolLunchReport/statistics/controller/dto/response/StatisticsResponse.java
+++ src/main/java/com/example/SchoolLunchReport/statistics/controller/dto/response/StatisticsResponse.java
@@ -1,6 +1,6 @@
 package com.example.SchoolLunchReport.statistics.controller.dto.response;
 
-import com.example.SchoolLunchReport.statistics.domain.type.PeriodType;
+import com.example.SchoolLunchReport.statistics.domain.boundary.type.PeriodType;
 import java.util.List;
 import lombok.Builder;
 
