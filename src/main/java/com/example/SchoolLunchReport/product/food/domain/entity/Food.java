ca6c7203982bc36efe30051bb3e1966c427089b4
0702Yoon <gkstkddbs99@naver.com>
2025-05-09 11:29:59 +0900

    [Refactor] Food 필드 카테고리 -> 카테고리, 서브 카테고리로 수정 및 Enumerated( EnumType.String) 처리

--- src/main/java/com/example/SchoolLunchReport/product/food/domain/entity/Food.java
+++ src/main/java/com/example/SchoolLunchReport/product/food/domain/entity/Food.java
@@ -1,23 +1,33 @@
 package com.example.SchoolLunchReport.product.food.domain.entity;
+
 import com.example.SchoolLunchReport.product.food.domain.type.Category;
+import com.example.SchoolLunchReport.product.food.domain.type.SubCategory;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
 @Entity
 @Getter
 @NoArgsConstructor
 public class Food {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
 
+    @Enumerated(value = EnumType.STRING)
     private Category category;
+
+    @Enumerated(value = EnumType.STRING)
+    private SubCategory subCategory;
 
     private String nutrition;
 
@@ -27,11 +37,12 @@ public class Food {
 
     @Builder
     public Food(String name, Category category, String nutrition, Double calorie,
-        String allergy) {
+        String allergy, SubCategory subCategory) {
         this.name = name;
         this.category = category;
         this.nutrition = nutrition;
         this.calorie = calorie;
         this.allergy = allergy;
+        this.subCategory = subCategory;
     }
 }
