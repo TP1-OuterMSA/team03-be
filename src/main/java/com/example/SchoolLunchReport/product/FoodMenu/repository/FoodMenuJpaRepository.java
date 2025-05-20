2796ae8469edf72a5034da76f3414efeb32626ad
0702Yoon <gkstkddbs99@naver.com>
2025-05-20 12:05:21 +0900

    [Feat] Food 이름으로 조회하기, FoodMenu 조회하기 추가

--- src/main/java/com/example/SchoolLunchReport/product/FoodMenu/repository/FoodMenuJpaRepository.java
+++ src/main/java/com/example/SchoolLunchReport/product/FoodMenu/repository/FoodMenuJpaRepository.java
@@ -1,11 +1,17 @@
 package com.example.SchoolLunchReport.product.FoodMenu.repository;
+
 import com.example.SchoolLunchReport.product.FoodMenu.domain.entity.FoodMenu;
+import com.example.SchoolLunchReport.product.food.domain.entity.Food;
 import com.example.SchoolLunchReport.product.menu.domain.entity.Menu;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 @Repository
 public interface FoodMenuJpaRepository extends JpaRepository<FoodMenu, Long> {
+
     List<FoodMenu> findByMenu(Menu menu);
+
+    Optional<FoodMenu> findByMenuAndFood(Menu menu, Food food);
 }
