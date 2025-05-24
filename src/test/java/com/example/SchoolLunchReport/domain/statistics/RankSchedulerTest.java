bdfb7af2c780616ca89eb31c2517f50af35f8530
0702Yoon <gkstkddbs99@naver.com>
2025-05-24 16:30:19 +0900

    [Build] 데이터 삽입용 test 삭제

--- src/test/java/com/example/SchoolLunchReport/domain/statistics/RankSchedulerTest.java
+++ src/test/java/com/example/SchoolLunchReport/domain/statistics/RankSchedulerTest.java
@@ -1,34 +1,34 @@
-package com.example.SchoolLunchReport.domain.statistics;
-
-import com.example.SchoolLunchReport.statistics.domain.boundary.type.PeriodType;
-import com.example.SchoolLunchReport.statistics.service.StatisticsFacade;
-import java.time.LocalDate;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-public class RankSchedulerTest {
-
-    @Autowired
-    private StatisticsFacade statisticsFacade;
-
-    @Test
-    public void 주간_랭크_계산_테스트() {
-        // 테스트용 날짜 설정
-        LocalDate registerDate = LocalDate.of(2025, 4, 6); // 일요일
-        LocalDate preDate = registerDate.minusWeeks(1);
-
-        statisticsFacade.calculateAndSaveRank(PeriodType.WEEKLY, registerDate);
-
-        // 결과 확인은 DB 확인 or 로그 확인
-    }
-
-    @Test
-    public void 월간_랭크_계산_테스트() {
-        LocalDate registerDate = LocalDate.of(2025, 4, 1); // 4월 1일
-        LocalDate preDate = registerDate.minusMonths(1);
-
-        statisticsFacade.calculateAndSaveRank(PeriodType.MONTHLY, registerDate);
-    }
-}
+//package com.example.SchoolLunchReport.domain.statistics;
+//
+//import com.example.SchoolLunchReport.statistics.domain.boundary.type.PeriodType;
+//import com.example.SchoolLunchReport.statistics.service.StatisticsFacade;
+//import java.time.LocalDate;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//public class RankSchedulerTest {
+//
+//    @Autowired
+//    private StatisticsFacade statisticsFacade;
+//
+//    @Test
+//    public void 주간_랭크_계산_테스트() {
+//        // 테스트용 날짜 설정
+//        LocalDate registerDate = LocalDate.of(2025, 4, 6); // 일요일
+//        LocalDate preDate = registerDate.minusWeeks(1);
+//
+//        statisticsFacade.calculateAndSaveRank(PeriodType.WEEKLY, registerDate);
+//
+//        // 결과 확인은 DB 확인 or 로그 확인
+//    }
+//
+//    @Test
+//    public void 월간_랭크_계산_테스트() {
+//        LocalDate registerDate = LocalDate.of(2025, 4, 1); // 4월 1일
+//        LocalDate preDate = registerDate.minusMonths(1);
+//
+//        statisticsFacade.calculateAndSaveRank(PeriodType.MONTHLY, registerDate);
+//    }
+//}
