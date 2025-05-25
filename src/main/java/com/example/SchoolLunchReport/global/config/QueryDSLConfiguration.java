29b1707f869a0eb6b3877863465982ebc428e39d
choieuntaek <choiet0225@gmail.com>
2025-05-25 19:25:16 +0900

    update error solution

--- src/main/java/com/example/SchoolLunchReport/global/config/QueryDSLConfiguration.java
+++ src/main/java/com/example/SchoolLunchReport/global/config/QueryDSLConfiguration.java
@@ -1,14 +1,14 @@
-package com.example.SchoolLunchReport.global.config;
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import jakarta.persistence.EntityManager;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class QueryDSLConfiguration {
-
-    @Bean
-    public JPAQueryFactory jpaQueryFactory(EntityManager entityManager) {
-        return new JPAQueryFactory(entityManager);
-    }
-}
+//package com.example.SchoolLunchReport.global.config;
+//import com.querydsl.jpa.impl.JPAQueryFactory;
+//import jakarta.persistence.EntityManager;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class QueryDSLConfiguration {
+//
+//    @Bean
+//    public JPAQueryFactory jpaQueryFactory(EntityManager entityManager) {
+//        return new JPAQueryFactory(entityManager);
+//    }
+//}
