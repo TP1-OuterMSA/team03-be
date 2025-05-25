7e0e5b575b4a317fc7febd337f037e52a1230c99
euntaek <108321588+euntaek4187@users.noreply.github.com>
2025-05-25 19:15:42 +0900

    Update Dockerfile (#100)
    * Update Dockerfile
    
    * Update QueryDSLConfiguration.java

--- src/main/java/com/example/SchoolLunchReport/global/config/QueryDSLConfiguration.java
+++ src/main/java/com/example/SchoolLunchReport/global/config/QueryDSLConfiguration.java
@@ -1,5 +1,4 @@
 package com.example.SchoolLunchReport.global.config;
-
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import org.springframework.context.annotation.Bean;
